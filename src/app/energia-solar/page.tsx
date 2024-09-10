4d060e7f88fae1074e59679ff9a4424d78983fbd
Maciel <112631220+macielrib@users.noreply.github.com>
2024-09-10 16:13:04 -0300

    Update page.tsx

--- src/app/energia-solar/page.tsx
+++ src/app/energia-solar/page.tsx
@@ -15,17 +15,14 @@ const EnergiaSolar = () => {
       >
      <Reveal>
      <div className="container mx-auto px-6 md:px-12 text-center md:text-left mt-16">
-          {/* Título */}
           <h1 className="text-4xl md:text-7xl font-baijam font-bold text-white mb-4">
           Energia Solar não é mais<br/> do futuro, <span className="text-yellow-500">é de hoje!</span>
           </h1>
 
-          {/* Descrição */}
           <p className="text-md md:text-lg text-white font-dmsans font-normal max-w-2xl">
           Enquanto todos ficam sem eletricidade devido a falhas, você ou sua empresa continuarão <b>sem preocupações!</b>
           </p>
 
-          {/* Botão de Contato */}
           <div className="mt-8">
             <Link href="https://wa.me/5548999999999" passHref legacyBehavior>
               <a className="inline-flex items-center gap-2 text-xl font-normal bg-yellow-500 py-3 px-6 rounded-md text-black font-dmsans transition-transform transform hover:scale-105">
