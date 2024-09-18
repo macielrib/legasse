6f7f2d326f125d82763239835e6b3f9fbf19baca
Maciel <112631220+macielrib@users.noreply.github.com>
2024-09-18 16:59:30 -0300

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,10 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Script from "next/script";
-import WhatsappButton from "./components/WhatsappButton"; // Importa o botão do WhatsApp
+import WhatsappButton from "./components/WhatsappButton";
+
+import { QueryClient, QueryClientProvider } from 'react-query'; // Importa QueryClient e QueryClientProvider
+import { useState } from 'react'; // Importa useState para manter o estado do QueryClient
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -55,6 +58,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // Cria o QueryClient uma vez ao inicializar o layout
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <html lang="pt-br">
       <head>
@@ -64,14 +70,17 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>
-        <Navbar />
-        {children}
-        <Footer />
-        <WhatsappButton /> 
-        <Script
-          src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"
-          strategy="lazyOnload"
-        />
+        {/* Envolvendo a aplicação com o QueryClientProvider */}
+        <QueryClientProvider client={queryClient}>
+          <Navbar />
+          {children}
+          <Footer />
+          <WhatsappButton />
+          <Script
+            src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"
+            strategy="lazyOnload"
+          />
+        </QueryClientProvider>
       </body>
     </html>
   );
