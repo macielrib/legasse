ba9a936753526b1b597b93bb11fcedd07c89a98f
Maciel <112631220+macielrib@users.noreply.github.com>
2024-09-18 16:56:06 -0300

    test

--- src/app/(root)/components/instagram-posts/page.tsx
+++ src/app/(root)/components/instagram-posts/page.tsx
@@ -1,40 +1,66 @@
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
+"use client";
 
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Image from "next/image";
+
+const getGraphHome = async () => {
+  const response = await axios.get('https://legasse-api.vercel.app/media/graph/home');
+
+  return response.data["media"].slice(0, 6);
+};
+
+const getGraphEnergy = async (id: string) => {
+  const response = await axios.get('https://legasse-api.vercel.app/media/graph/energy'
+  );
+
+  return response.data["media"].slice(0, 6);
+};
 interface InstagramPost {
   id: string;
-  media_type: 'IMAGE' | 'VIDEO';
+  media_type: "IMAGE" | "VIDEO";
   media_url: string;
   thumbnail_url?: string;
   permalink: string;
 }
 
 const InstagramSection: React.FC = () => {
-  const [legasseHomePosts, setLegasseHomePosts] = useState<InstagramPost[]>([]);
-  const [legasseEnergyPosts, setLegasseEnergyPosts] = useState<InstagramPost[]>([]);
+  // const [legasseHomePosts, setLegasseHomePosts] = useState<InstagramPost[]>([]);
+  // const [legasseEnergyPosts, setLegasseEnergyPosts] = useState<InstagramPost[]>([]);
 
-  const fetchInstagramPosts = async () => {
-    try {
-      const homeResponse = await fetch('https://legasse-api.vercel.app/media/graph/home');
-      const homeData = await homeResponse.json();
-      setLegasseHomePosts(homeData.media.slice(0, 6)); 
+  const graphHome = useQuery({
+    queryKey: ["instagram-media-home"],
+    queryFn: async () => await getGraphHome(),
+  });
 
-      const energyResponse = await fetch('https://legasse-api.vercel.app/media/graph/energy');
-      const energyData = await energyResponse.json();
-      setLegasseEnergyPosts(energyData.media.slice(0, 6));
-    } catch (error) {
-      console.error("Erro ao buscar posts do Instagram:", error);
-    }
-  };
+  // const fetchInstagramPosts = async () => {
+  //   try {
+  //     const homeResponse = await fetch('https://legasse-api.vercel.app/media/graph/home');
+  //     const homeData = await homeResponse.json();
+  //     setLegasseHomePosts(homeData.media.slice(0, 6));
 
-  useEffect(() => {
-    fetchInstagramPosts();
-  }, []);
+  //     const energyResponse = await fetch('https://legasse-api.vercel.app/media/graph/energy');
+  //     const energyData = await energyResponse.json();
+  //     setLegasseEnergyPosts(energyData.media.slice(0, 6));
+  //   } catch (error) {
+  //     console.error("Erro ao buscar posts do Instagram:", error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchInstagramPosts();
+  // }, []);
 
   const renderPost = (post: InstagramPost) => {
     return (
-      <a href={post.permalink} target="_blank" rel="noopener noreferrer" key={post.id} className="relative block w-full h-0 pb-[100%] overflow-hidden">
-        {post.media_type === 'VIDEO' ? (
+      <a
+        href={post.permalink}
+        target="_blank"
+        rel="noopener noreferrer"
+        key={post.id}
+        className="relative block w-full h-0 pb-[100%] overflow-hidden"
+      >
+        {post.media_type === "VIDEO" ? (
           <video
             src={post.media_url}
             controls
@@ -43,12 +69,11 @@ const InstagramSection: React.FC = () => {
         ) : (
           <Image
             src={post.media_url}
-            alt={`Post ${post.id}`}
+          alt="a"
             layout="fill"
             objectFit="cover"
             className="absolute top-0 left-0"
-          />
-        )}
+          />)}
       </a>
     );
   };
@@ -58,11 +83,13 @@ const InstagramSection: React.FC = () => {
       <div className="container mx-auto">
         <div className="text-left mb-12">
           <h2 className="text-4xl md:text-7xl font-baijam font-bold mb-4">
-            Conecte-se conosco:<br />
+            Conecte-se conosco:
+            <br />
             <span className="text-yellow-500">Siga-nos</span> no instagram!
           </h2>
           <p className="text-md md:text-xl font-dmsans font-medium text-black max-w-2xl">
-            Confira as últimas novidades e acompanhe nossos projetos<br /> nas nossas <b>contas no Instagram.</b>
+            Confira as últimas novidades e acompanhe nossos projetos
+            <br /> nas nossas <b>contas no Instagram.</b>
           </p>
         </div>
 
@@ -71,14 +98,26 @@ const InstagramSection: React.FC = () => {
           <div className="space-y-8">
             <div className="flex items-center space-x-4">
               <span className="py-2 p-2 rounded-md bg-neutral-900">
-                <Image src={'/logo.png'} width={18} height={18} alt="Logo Instagram" />
+                <Image
+                  src={"/logo.png"}
+                  width={18}
+                  height={18}
+                  alt="Logo Instagram"
+                />
               </span>
               <span className="text-black font-baijam text-2xl font-bold">
-                <a href="https://instagram.com/legasseenergia" target="_blank" rel="noopener noreferrer" className="transition duration-300 hover:translate-x-1 hover:text-yellow-500">@legasseenergia</a>
+                <a
+                  href="https://instagram.com/legasseenergia"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition duration-300 hover:translate-x-1 hover:text-yellow-500"
+                >
+                  @legasseenergia
+                </a>
               </span>
             </div>
             <div className="grid grid-cols-2 gap-4">
-              {legasseEnergyPosts.map(renderPost)}
+              {graphHome.data.map(renderPost)}
             </div>
           </div>
 
@@ -86,14 +125,26 @@ const InstagramSection: React.FC = () => {
           <div className="space-y-8">
             <div className="flex items-center space-x-2">
               <span className="py-2 p-2 rounded-md bg-black">
-                <Image src={'/logo.png'} width={18} height={18} alt="Logo Instagram" />
+                <Image
+                  src={"/logo.png"}
+                  width={18}
+                  height={18}
+                  alt="Logo Instagram"
+                />
               </span>
               <span className="text-black font-baijam text-2xl font-bold">
-                <a href="https://instagram.com/legassehome" target="_blank" rel="noopener noreferrer" className="transition duration-300 hover:translate-x-1 hover:text-yellow-500">@legassehome</a>
+                <a
+                  href="https://instagram.com/legassehome"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition duration-300 hover:translate-x-1 hover:text-yellow-500"
+                >
+                  @legassehome
+                </a>
               </span>
             </div>
             <div className="grid grid-cols-2 gap-4">
-              {legasseHomePosts.map(renderPost)}
+              {graphHome.data.map(renderPost)}
             </div>
           </div>
         </div>
