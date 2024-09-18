1109d1bd934f09c00c5df49114bfb658079d52de
fabricioaquiles <fab.aquiles123@gmail.com>
2024-09-18 17:43:39 -0300

    .

--- src/app/(root)/components/instagram-posts/page.tsx
+++ src/app/(root)/components/instagram-posts/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 interface InstagramPost {
@@ -54,11 +55,12 @@ const InstagramSection: React.FC = () => {
             className="absolute top-0 left-0 w-full h-full object-cover"
           />
         ) : (
-          <img
+          <Image
             src={post.media_url}
+            overrideSrc={post.media_url}
             alt={`Post ${post.id}`}
-            // layout="fill"
-            // objectFit="cover"
+            layout="fill"
+            objectFit="cover"
             className="absolute top-0 left-0"
           />
         )}
