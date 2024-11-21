8698d4729e074d5fd0387afa7604357dfa388154
Maciel <112631220+macielrib@users.noreply.github.com>
2024-11-21 19:43:40 -0300

    Add Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Script from "next/script";
@@ -72,6 +73,8 @@ export default function RootLayout({
           src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"
           strategy="lazyOnload"
         />
+
+        <Analytics />
       </body>
     </html>
   );
